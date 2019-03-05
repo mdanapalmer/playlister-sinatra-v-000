bfcc5772be69589377a0399fa6772e2d66c320a3
Dana Palmer <roiai4@hotmail.com>
2019-03-05 00:05:33 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,5 @@
+require 'rack-flash'
+
 class SongsController < ApplicationController
 
   get '/songs' do
@@ -5,9 +7,38 @@ class SongsController < ApplicationController
     erb :'/songs/index'
   end
   
+  get '/songs/new' do
+    erb :'/songs/new'
+  end
+  
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'songs/show'
   end
+  
+  post '/songs' do
+  @song = Song.create(:name => params["Name"])
+  @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+  @song.genre_ids = params[:genres]
+  @song.save
+
+  erb :'songs/show', locals: {message: "Successfully created song."}
+  
+  redirect("/songs/#{@song.slug}")
+  end
+  
+  get '/songs/:slug/edit' do
+   @song = Song.find_by_slug(params[:slug])
+   erb :'songs/edit'
+  end
+
+  patch '/songs/:slug' do
+  @song = Song.find_by_slug(params[:slug])
+  @song.update(params[:song])
+  @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+  @song.save
+  erb :'songs/show', locals: {message: "Successfully updated song."}
+    redirect("/songs/#{@song.slug}")
+end
 
 end
